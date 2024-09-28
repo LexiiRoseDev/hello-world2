a239f357bb7fc51c073bba1d3f6c1627c26f2640
LexiiRoseDev <100715094@unimail.derby.ac.uk>
2024-09-29 00:41:16 +0100

    Update 100-days-of-code.py

--- 100-days-of-code.py
+++ 100-days-of-code.py
@@ -3,3 +3,12 @@
 # Write a multi-line comment
 
 print("Hello World")
+
+# Day 2 Task
+# Create variables for your name, age and favourite colour and print them.
+
+name = "Lexii"
+age = 30
+colour = "blue"
+
+print(name, age, colour)
