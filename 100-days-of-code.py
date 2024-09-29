0ed2e3bfca00c3d37c1c25fc683743fdef8f37ea
LexiiRoseDev <100715094@unimail.derby.ac.uk>
2024-09-29 22:41:25 +0100

    Update 100-days-of-code.py

--- 100-days-of-code.py
+++ 100-days-of-code.py
@@ -1,14 +1,25 @@
+# -------------------------------------------------------------------------------------------------------------
 # Day 1 Task
 # Write a line of code that prints "Hello World"
 # Write a multi-line comment
 
-print("Hello World")
-
+# print("Hello World")
+# -------------------------------------------------------------------------------------------------------------
 # Day 2 Task
 # Create variables for your name, age and favourite colour and print them.
 
-name = "Lexii"
-age = 30
-colour = "blue"
+# name = "Lexii"
+# age = 30
+# colour = "blue"
 
-print(name, age, colour)
+# print(name, age, colour)
+# -------------------------------------------------------------------------------------------------------------
+# Day 3 Task
+# Write a program that reads user input and print it to the console.
+# Modify the program to read and print different data type inputs (integers, floating-point numbers, strings)
+
+name = str(input("Please enter your name: "))
+age = int(input("Please enter your age: "))
+weight = float(input("Please enter your weight: "))
+
+print(f"Hello, {name}. You are {age} years old, and you weigh {weight}.")
