e14f61b6e50bab1fc8b7b82a7f83c7eab4de6f5c
LexiiRoseDev <100715094@unimail.derby.ac.uk>
2024-10-01 16:59:41 +0100

    Create week2-tasks.py

---
+++ week2-tasks.py
@@ -0,0 +1,46 @@
+# --------------------------------------------------------------------------------
+# Task 1
+
+# pi = 3.14
+# radius = 6
+# circumference = 2 * pi * radius
+# print(f"The circle with radius = {radius} has circumference = {circumference}")
+
+# Reassign radius to the value of 15
+# Reassign circumference to the circumference of the circle with radius 15
+# In addition to the existing print statement, print out the new circumference.
+
+# radius = 15
+# circumference = 2 * pi * radius
+# print(f"The circle with radius = {radius} has circumference = {circumference}")
+# --------------------------------------------------------------------------------
+# Task 2
+
+# DO NOT TOUCH THE FOLLOWING LINES 4, 5 and 6
+# THESE ARE USED FOR THE TESTS. 
+
+# a = int(input("Please enter a number: ")) # leave this line alone
+# b = int(input("Please enter a number: ")) # leave this line alone
+# c = int(input("Please enter a number: ")) # leave this line alone
+
+# TASK 1. 
+# print out the type of 10.3 + 5
+
+# print(type(10.3+5))
+
+# TASK 2.
+# print(a + b * c) # Correct this so that the ``+`` is evaluated first
+# print(a**b+c)    # Correct this so that the exponent is b+c
+
+# print((a + b) * c)
+
+# TASK 3. 
+# print the output of a < b
+
+# print(a < b)
+
+# TASK 4. 
+# print the output of (a < b) == (a <= b)
+
+# print((a < b) == (a <= b))
+# --------------------------------------------------------------------------------
