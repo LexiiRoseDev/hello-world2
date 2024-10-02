50522d1fde056786cb31f196f4f0a6d32534cc9f
LexiiRoseDev <100715094@unimail.derby.ac.uk>
2024-10-02 17:44:30 +0100

    Update 100-days-of-code.py

--- 100-days-of-code.py
+++ 100-days-of-code.py
@@ -18,8 +18,34 @@
 # Write a program that reads user input and print it to the console.
 # Modify the program to read and print different data type inputs (integers, floating-point numbers, strings)
 
-name = str(input("Please enter your name: "))
-age = int(input("Please enter your age: "))
-weight = float(input("Please enter your weight: "))
+# name = str(input("Please enter your name: "))
+# age = int(input("Please enter your age: "))
+# weight = float(input("Please enter your weight: "))
 
-print(f"Hello, {name}. You are {age} years old, and you weigh {weight}.")
+# print(f"Hello, {name}. You are {age} years old, and you weigh {weight}.")
+# -------------------------------------------------------------------------------------------------------------
+# Day 4 Task
+# Write a program that declares two integer variables and perform basic arithmetic operations
+# (addition, subtraction, multiplication, division) on them. Print the results to the console.
+
+# a = 5
+# b = 4
+# c = a + b
+# print(f"{a} + {b} = {c}")
+# c = a - b
+# print(f"{a} - {b} = {c}")
+# c = a * b
+# print(f"{a} * {b} = {c}")
+# c = a / b
+# print(f"{a} / {b} = {c}")
+
+# Write a program that calculates the area of a rectangle. Prompt the user to input the length (integer)
+# of the rectangle, calculate the area (length*width), and print the result.
+
+# w = 10
+# l = int(input("Please enter the length of the rectangle: "))
+# a = w*l
+# print(f"The area of your rectangle is {a}.")
+
+# Modify the above program to read decimal numbers as the length and width, and output the area to two
+# decimal points.
